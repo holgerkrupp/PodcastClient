c88cc6222f765ef6448096f9e800c82250d6acc0
Holger Krupp <email@holgerkrupp.de>
2023-12-01 20:09:21 +0100

    Starting with Views

---
+++ PodcastClient/PodcastClasses/Playlist.swift
@@ -0,0 +1,20 @@
+//
+//  Playlist.swift
+//  PodcastClient
+//
+//  Created by Holger Krupp on 01.12.23.
+//
+
+import Foundation
+
+class Playlist{
+    var deleteable:Bool = true // to enable standard lists like "play next queue" or similar that can't be deleted by the user
+    var name: String?
+    var items: [PlaylistEntry]? // we need to ensure that we can create an ordered list. Swiftdata won't ensure that the items are kept in the same order without manually managing that.
+}
+
+class PlaylistEntry {
+    var episode: Episode?
+    var dateAdded: Date?
+    var order:Int?
+}
