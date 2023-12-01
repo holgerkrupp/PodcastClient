da6c5a9c9f56dde9c46b596e42377d10d406cdb4
Holger Krupp <email@holgerkrupp.de>
2023-12-01 20:09:21 +0100

    Starting with Views

---
+++ PodcastClient/Views/PodcastList.swift
@@ -0,0 +1,18 @@
+//
+//  PodcastList.swift
+//  PodcastClient
+//
+//  Created by Holger Krupp on 01.12.23.
+//
+
+import SwiftUI
+
+struct PodcastList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PodcastList()
+}
