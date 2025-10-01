53f5ce69443aebcd5c49b3dd0f72654460354017
Holger Krupp <email@holgerkrupp.de>
2025-10-01 13:03:14 +0200

    Refined Import & Subscription

--- Raul/Views/Search/HotPodcastView.swift
+++ Raul/Views/Search/HotPodcastView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 import fyyd_swift
 
 struct HotPodcastView: View {
-    @ObservedObject  var viewModel : PodcastSearchViewModel
+  
     @Environment(\.modelContext) private var context
-    
+    @StateObject private var viewModel = PodcastSearchViewModel()
     
     var body: some View {
-        
-        
+        List{
+        Group{
+            if !viewModel.languages.isEmpty {
+                Picker("Language", selection: $viewModel.selectedLanguage) {
+                    ForEach(viewModel.languages, id: \.self) { name in
+                        Text(name.languageName()).tag(name)
+                    }
+                }
+                .pickerStyle(.menu)
+            } else {
+                ProgressView("Loading languages...")
+            }
+        }
+        .padding()
+        .listRowSeparator(.hidden)
+        .listRowBackground(Color.clear)
+        .listRowInsets(.init(top: 0,
+                             leading: 0,
+                             bottom: 0,
+                             trailing: 0))
         
         
         
@@ -23,27 +41,34 @@ struct HotPodcastView: View {
      
             if viewModel.isLoading {
                 ProgressView()
+                    .listRowSeparator(.hidden)
+                    .listRowBackground(Color.clear)
+                    .listRowInsets(.init(top: 0,
+                                         leading: 0,
+                                         bottom: 0,
+                                         trailing: 0))
             } else {
                 
                
-                ForEach(viewModel.hotPodcasts , id: \.self) { podcast in
-                    SubscribeToPodcastView(newPodcastFeed: podcast)
-                        .modelContext(context)
-                        .listRowSeparator(.hidden)
-                        .listRowBackground(Color.clear)
-                        .listRowInsets(.init(top: 0,
-                                             leading: 0,
-                                             bottom: 0,
-                                             trailing: 0))
-              
-                    
+                    ForEach(viewModel.hotPodcasts , id: \.self) { podcast in
+                        SubscribeToPodcastView(newPodcastFeed: podcast)
+                            .modelContext(context)
+                            .listRowSeparator(.hidden)
+                            .listRowBackground(Color.clear)
+                            .listRowInsets(.init(top: 0,
+                                                 leading: 0,
+                                                 bottom: 0,
+                                                 trailing: 0))
+                        
+                        
+                    }
                 }
-                
-                .listStyle(.plain)
-                .navigationTitle("Hot")
+
                 
                 
             }
+        .listStyle(.plain)
+        .navigationTitle("Hot Podcasts")
             
             
         
