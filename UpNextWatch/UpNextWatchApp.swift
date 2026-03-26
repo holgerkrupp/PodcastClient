ea925a8b828a0ea4ad0f570b0bf93480c0e17740
Holger Krupp <email@holgerkrupp.de>
2026-03-26 17:34:05 +0900

    Playback on watch OS and optimized settings.

--- UpNextWatch/UpNextWatchApp.swift
+++ UpNextWatch/UpNextWatchApp.swift
@@ -2,12 +2,23 @@ import SwiftUI
 
 @main
 struct UpNextWatchApp: App {
+    @Environment(\.scenePhase) private var scenePhase
     @StateObject private var store = WatchSyncStore()
+    @StateObject private var playbackController = WatchPlaybackController()
 
     var body: some Scene {
         WindowGroup {
             WatchRootView()
                 .environmentObject(store)
+                .environmentObject(playbackController)
+                .task {
+                    playbackController.attach(store: store)
+                }
+                .onChange(of: scenePhase) { _, newPhase in
+                    if newPhase != .active {
+                        playbackController.flushProgress()
+                    }
+                }
         }
     }
 }
